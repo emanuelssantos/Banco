469d345ff7d9f276b19a54e26bf9af5ff203c03c
emanuelssantos <emanueldasilvasantos@gmail.com>
2020-10-22 19:58:00 -0300

    add all

--- src/banco/Banco.java
+++ src/banco/Banco.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package banco;
 
 import javafx.application.Application;
@@ -10,8 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-//import banco.Classes.*;
 
 /**
  *
---
+++ src/banco/TelaCadastroController.java
@@ -0,0 +1,28 @@
+/*
+----------------NÃO FUNCIONA--------------
+ */
+package banco;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+/**
+ * FXML Controller class
+ *
+ * @author emanuel
+ */
+public class TelaCadastroController implements Initializable {
+
+    @FXML
+    private Text txt;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
